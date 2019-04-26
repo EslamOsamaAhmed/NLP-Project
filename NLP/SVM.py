027a1fe68ff414e8b6a56cf42b5f7f2675bbcd35
Eslam Osama Ahmed <37809837+EslamOsamaAhmed@users.noreply.github.com>
2019-04-26 19:09:20 +0200

    edit
    edit

--- NLP/SVM.py
+++ NLP/SVM.py
@@ -27,12 +27,12 @@ import nltk
 import random
 from sklearn.metrics import classification_report
 
-datasetname = "DataSets/dataset-3.csv"
+datasetname = "DataSets/Dataset-1Charts/dataset-1.csv"
 
 data = pd.read_csv(datasetname,encoding='latin-1')
 data.head()
 
-if(datasetname == "DataSets/dataset-2.csv"):
+if(datasetname == "DataSets/Dataset-2Charts/dataset-2.csv"):
    data=data.drop(["Unnamed: 2", "Unnamed: 3", "Unnamed: 4"], axis=1)
 
 data=data.rename(columns={"v1":"class", "v2":"text"})
@@ -89,67 +89,3 @@ pickle.dump(svc, open(filename, 'wb'))
 
 # load the model from disk
 loaded_model = pickle.load(open(filename, 'rb'))
-
-#def pre_process(text):
-#    
-#    text = text.translate(str.maketrans('', '', string.punctuation))
-#    text = [word for word in text.split() if word.lower() not in stopwords.words('english')]
-#    words = ""
-#    for i in text:
-#            stemmer = SnowballStemmer("english")
-#            words += (stemmer.stem(i))+" "
-#    return words
-#
-## GUI Window to Enter Email to Detect it
-#from tkinter import *
-#root=Tk()
-#root.resizable(width=False, height=False)
-#root.geometry("200x130")
-#
-#Label1 = Label(root, text = "Enter Email") 
-#EmailField = Entry(root,text = "Email...", width = 100)
-#DetectButton = Button(root, text = "Detect")
-#
-#def leftClick(event):
-#    Email = "Your free ringtone is waiting to be collected. Simply text the password \MIX\" to 85069 to verify. Get Usher and Britney. FML	 PO Box 5249 MK17 92H. 450Ppw 16"	
-#    
-#    #Prprocessing for Data to find the best Acc and Vectorizing the data to be able to be learned
-#    textFeatures = Email
-#    textFeatures = pre_process(textFeatures)
-#    vectorizer = TfidfVectorizer("english")
-#    x = [textFeatures]
-#    NEmailfeatures = vectorizer.fit_transform(x)
-#    
-#    NEmailfeatures = NEmailfeatures.toarray()
-#
-#    row = []    
-#    col = []
-#    data = []
-#    (fd,sd) = NEmailfeatures.shape
-#    
-#    for x in range(sd):
-#        row.append(0)
-#        
-#    for y in range(sd,8037):
-#        row.append(1)
-#    
-#    for z in range(8037):
-#        col.append(z)
-#        
-#    for f in range(sd):
-#        data.append(NEmailfeatures[0][f])
-#        
-#    for f in range(sd,8037):
-#        data.append(2)
-#        
-#    EmailEntry = csr_matrix((data, (row, col)), shape=(2, 8037))
-#    
-#    Nprediction = loaded_model.predict(EmailEntry)
-#    print(Nprediction[0])
-#
-#Label1.pack()
-#EmailField.pack(ipady=30)
-#DetectButton.pack()
-#DetectButton.bind("<Button-1>", leftClick)
-#
-#root.mainloop()
